206d45b3ab28e5ace44170bf5561884a6235df00
asnan-xrz <asnanbevroor@gmail.com>
2023-12-14 15:52:36 +0700

    add frontend eas-pemweb

---
+++ messengger-nuxt/components/RegisterDialog.vue
@@ -0,0 +1,59 @@
+<!-- RegisterDialog.vue -->
+
+<template>
+    <div class="register-dialog">
+      <form @submit="onSubmit">
+        <label for="username">Username:</label>
+        <input v-model="username" type="text" required />
+  
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RegisterDialog',
+    data: () => ({
+      username: ''
+    }),
+    methods: {
+      onSubmit(event) {
+        event.preventDefault();
+        // Emit an event to the parent component with the entered username
+        this.$emit('submit', event, this.username);
+        // You may perform additional registration/authentication logic here
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .register-dialog {
+    max-width: 300px;
+    margin: auto;
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  label {
+    margin-bottom: 0.5rem;
+  }
+  
+  input {
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+  }
+  
+  button {
+    padding: 0.5rem;
+    background-color: #2a60ff;
+    color: white;
+    border: none;
+    cursor: pointer;
+  }
+  </style>
+  
---
+++ messengger-nuxt/pages/status.vue
@@ -0,0 +1,40 @@
+<!-- /pages/status.vue -->
+
+<template>
+    <div>
+      <h1>Cek Status Pendaftaran</h1>
+      <form @submit.prevent="checkStatus">
+        <label for="email">Email:</label>
+        <input v-model="email" type="email" id="email" required />
+  
+        <button type="submit">Check Status</button>
+      </form>
+  
+      <!-- Display status here -->
+      <div v-if="status !== null">
+        <p>Status Pendaftaran: {{ status }}</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        status: null,
+      };
+    },
+    methods: {
+      checkStatus() {
+        // Implement logic to fetch and display status based on the email
+        console.log('Checking status for email:', this.email);
+  
+        // Example: Assume you have an API endpoint to fetch status
+        // Replace the following code with actual API call
+        this.status = 'Waiting'; // Set status based on API response
+      },
+    },
+  };
+  </script>
+  
